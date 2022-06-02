4c594c6dfdac3b5acb11655115b954995d34363e
adil waheed <adilwaheed2222.com@gmail.com>
2022-06-02 23:59:24 +0500

    Adding lab7 task 5,7

--- Web_lab/lab7/task5.js
+++ Web_lab/lab7/task5.js
@@ -1,21 +1,8 @@
-// 5.	Write a javascript arrow function named calculateSupply that:
-// a.	takes 2 arguments: age, amount per day.
-// b.	calculates the amount consumed for rest of the life (based on a constant max age).
-// c.	outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
-var age=document.getElementById('input');
-var amount=document.getElementById('input2');
-function consuming(){
-    if(age.value.length>0 && amount.value.length>0)
-        {
-            consumed(age.value,amount.value);
-        }
-        age.value='';
-        amount.value='';
+var result=document.getElementById('input');
+function check(){ 
+    const numbers = [1,-5,7,9,-10,15,22,-65,-120]; 
+    result.value=numbers.filter(Ispositive);
 }
-var consumed=(age,amount)=>
-    {        
-           var mxage=60;
-           var consume=mxage*12*30*amount;
-           var current=age*12*30*amount;
-           alert(`Current Consumed Amount is ${current} You  need ${consume-current} to last you until the ripe old age of ${mxage}`)       
+function Ispositive(num) {
+return num > 0;
 }
--- Web_lab/lab7/task7.js
+++ Web_lab/lab7/task7.js
@@ -1,2 +1,19 @@
-
-var txtarea=document.getElementById('input');
+var txtarea = document.getElementById('input');
+class Member {
+    constructor(name, age, salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
+class Employee extends Member {
+    constructor(name, age, salary, department) {
+        super(name, age, salary);
+        this.department = department;
+    }
+    printEmpDetails()    
+    { txtarea.value=`The Employee details are :Name= ${this.name} ,Age= ${this.age},Salary= ${this.salary},Department= ${this.department}`
+     }
+} 
+const a = new Employee('Adil Waheed', 21, 25000, 'BSE');
+a.printEmpDetails();
